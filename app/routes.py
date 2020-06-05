dcd30072fe10bbcb1008a9277c7a4a164c5db088
Siddhanth J Ajri <y2jsiddajri@gmail.com>
2020-06-06 04:23:22 +0530

    basic functionality of pokedex done

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,6 @@ import os
 @app.route('/')
 @app.route('/index')
 def index():
-    f = open("./static/cache/api-cache.txt", "r")
+    f = open("api-cache.txt", "r")
     pokedex_dict = ast.literal_eval(f.read())
-    return render_template('index.html', pokedex = pokedex_dict)
+    return render_template('index.html', pokedex = pokedex_dict, entries = len(pokedex_dict)+1)
