1963efeaabfa33635eb9d8df32211befe0d60b4f
Siddhanth J Ajri <y2jsiddajri@gmail.com>
2020-06-06 03:11:36 +0530

    checking for simple access to cached api data

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
 from app import app
 from flask import render_template, url_for
+import requests
+import json
+import ast
+import os
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    f = open("./static/cache/api-cache.txt", "r")
+    pokedex_dict = ast.literal_eval(f.read())
+    return render_template('index.html', pokedex = pokedex_dict)
