e74220502443a71a4c07d0b50a28d14b1840be14
Siddhanth J Ajri <y2jsiddajri@gmail.com>
2020-06-22 04:26:01 +0530

    cached all pokemon, can be used

--- caching-pokeapi-data.py
+++ caching-pokeapi-data.py
@@ -15,7 +15,7 @@ if os.path.exists("api-cache.txt"):
     pokedex_dict = ast.literal_eval(f.read())
 
 else:
-    for i in range(1,387): #All pokemon in Kanto region = 1 (Bulbasaur) to 386 (Deoxys)
+    for i in range(1,808): #All pokemon in Kanto region = 1 (Bulbasaur) to 807 (Zeraora) ==> total of 807 Pokemon exist
         pokemon = requests.get('https://pokeapi.co/api/v2/pokemon/'+str(i)).json()
         pokedex_dict[i] = {}
         name = pokemon['name']
@@ -41,13 +41,13 @@ print("PokeDex of length:")
 print(len(pokedex_dict))
 
 ## TESTING
-print("Testing for Mew:")
+print("Testing for Zeraora:")
 
-print(pokedex_dict[151]['name'])
-print(pokedex_dict[151]['img_url'])
-print(pokedex_dict[151]['id'])
-if len(pokedex_dict[151]['type'])>1:
-    print(pokedex_dict[151]['type'][0])
-    print(pokedex_dict[151]['type'][1])
+print(pokedex_dict[807]['name'])
+print(pokedex_dict[807]['img_url'])
+print(pokedex_dict[807]['id'])
+if len(pokedex_dict[807]['type'])>1:
+    print(pokedex_dict[807]['type'][0])
+    print(pokedex_dict[807]['type'][1])
 else:
-    print(pokedex_dict[151]['type'][0])
+    print(pokedex_dict[807]['type'][0])
